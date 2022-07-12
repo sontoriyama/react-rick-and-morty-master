9d472212d0e388db1437a646e99a6a3f9f590a8b
songokucoder <miscodices@gmail.com>
2022-07-12 04:31:10 +0200

    pujar a gitpages amb vite!

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://faztweb.github.io/react-rick-and-morty/",
+  base: "https://sontoriyama.github.io/react-rick-and-morty/",
   plugins: [react(), eslint()],
 });
